3f211390d88a9a3f0590bdd5a60f555e0dfe53cb
Ivan Kuritsyn <vanya.lp@yandex.ru>
2024-05-30 14:03:14 +0300

    Тестирование первого коммита

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+let number = 5;
+const leftBorderWith = 1;
+
+//leftBorderWith = 29;
+//console.log(leftBorderWith)
+
+
+let obs = {
+    a: 50
+};
+
+obs.a = 40;
+let obdds;
+console.log(obdds);
+
+let m = new Map();
+
+m.set("brother", 20);
+m = m.entries();
+
+let m1 = [["brother", 20]];
+
+console.log(m1 == m)
+
+let mm = {
+    _: 10
+}
+console.log(mm._)
+
+
+let checker = confirm("Are u ready?");
+console.log(typeof (checker) )
