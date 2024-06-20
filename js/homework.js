5cbcd9ac37f4a7cfd9e5f4c90d317bc1f4211cd1
Ivan Kuritsyn <vanya.lp@yandex.ru>
2024-06-20 09:42:15 +0300

    newTest

--- js/homework.js
+++ js/homework.js
@@ -32,20 +32,18 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const movies = {};
 for (let i = 0; i < 2; i++) {
 const lastWatchedMovie = prompt('Один из последних просмотренных фильмов?','');
 const moviePersonalRating = prompt('На сколько оцените его?','');
-movies[lastWatchedMovie] = moviePersonalRating;
+personalMovieDB.movies[lastWatchedMovie] = moviePersonalRating;
 }
 
-for (let key in personalMovieDB) {
+for (let key in personalMovieDB.movies) {
     console.log(key);
-    console.log(personalMovieDB[key]);
+    console.log(personalMovieDB.movies[key]);
 }
 
-console.log(personalMovieDB);
-console.log(movies); */
+console.log(personalMovieDB); */
 
 /* Задание на урок 2:
 
@@ -64,7 +62,7 @@ str.length - и получить её длину)
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+/* const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -72,7 +70,6 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const movies = {};
 for (let i = 0; i < numberOfFilms; i++) {
     let lastWatchedMovie = prompt('Один из последних просмотренных фильмов?','');
     while (lastWatchedMovie == null || lastWatchedMovie == '' || lastWatchedMovie.length > 50 ) {
@@ -82,15 +79,100 @@ for (let i = 0; i < numberOfFilms; i++) {
     while (moviePersonalRating == null || moviePersonalRating == '') {
         moviePersonalRating = prompt('Значение недопустимо, введите еще раз','');
     }
-    movies[lastWatchedMovie] = moviePersonalRating;
+    personalMovieDB.movies[lastWatchedMovie] = moviePersonalRating;
 }
 
 personalMovieDB.count < 10?console.log("Просмотрено довольно мало фильмов"):personalMovieDB.count < 30?console.log("Вы классический зритель"):personalMovieDB.count >= 30?console.log("Вы киноман"):console.log("Произошла ошибка");
 
-for (let key in personalMovieDB) {
+for (let key in personalMovieDB.movies) {
     console.log(key);
-    console.log(personalMovieDB[key]);
+    console.log(personalMovieDB.movies[key]);
 }
 
-console.log(personalMovieDB);
-console.log(movies);
+console.log(personalMovieDB); */
+
+
+/* Задание на урок 3:
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно */
+
+let numberOfFilms;
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    console.log(numberOfFilms);
+    while(Number.isNaN(numberOfFilms) || numberOfFilms === '' || numberOfFilms <= 0){
+        numberOfFilms = +prompt('Введите еще раз', '');
+        console.log(numberOfFilms);
+    }
+}
+start();
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+function filmsCollector(){
+    for (let i = 0; i < numberOfFilms; i++) {
+        let lastWatchedMovie = prompt('Один из последних просмотренных фильмов?','');
+        while (lastWatchedMovie == null || lastWatchedMovie == '' || lastWatchedMovie.length > 50 ) {
+            lastWatchedMovie = prompt('Значение недопустимо, введите еще раз','');
+        }
+        let moviePersonalRating = prompt('На сколько оцените его?','');
+        while (moviePersonalRating == null || moviePersonalRating == '') {
+            moviePersonalRating = prompt('Значение недопустимо, введите еще раз','');
+        }
+        personalMovieDB.movies[lastWatchedMovie] = moviePersonalRating;
+    } 
+}
+
+function userRatingChecker(){
+    personalMovieDB.count < 10?console.log("Просмотрено довольно мало фильмов"):personalMovieDB.count < 30?console.log("Вы классический зритель"):personalMovieDB.count >= 30?console.log("Вы киноман"):console.log("Произошла ошибка");
+}
+
+function showMyDB(hidden){
+    if (hidden === false){
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres(){
+    for (let i = 1; i <= 3; i++){
+        let genreReq = prompt(`Ваш любимый жанр под номером ${i}`,'');
+        personalMovieDB.genres.push(genreReq);
+        console.log(personalMovieDB.genres);
+    }
+}
+showMyDB(personalMovieDB.privat);
+writeYourGenres();
+
+
+
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
